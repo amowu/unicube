83171f5b74ca70b8b3a2254bdee5b2adac907dbb
Amo Wu <amowu@unicube.com.tw>
2014-02-06 15:22:02 +0800

    Add 'web-server'.

---
+++ web-server/package.js
@@ -0,0 +1,10 @@
+{
+  "name": "web-server",
+  "description": "Unicube Web Server",
+  "version": "0.1.0",
+  "author": "Amo Wu",
+  "private": true,
+  "dependencies": {
+    "express": "3.4.7"
+  }
+}
---
+++ web-server/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+  // Configuration
+  app.configure(function() {
+  app.use(express.static('../web-client'));
+});
+
+app.listen(80);
+console.log('===============================');
+console.log('Web server listening on port 80');
+console.log('===============================');
