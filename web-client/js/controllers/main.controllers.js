9c05213dcc2ea51449964b4aacdea5f1a3b4d80b
Amo Wu <amo260@gmail.com>
2014-02-26 00:46:26 +0800

    1. 更新 job list. 2. 修正 anchor.

--- web-client/js/controllers/main.controllers.js
+++ web-client/js/controllers/main.controllers.js
@@ -1,5 +1,9 @@
 var mainController = angular.module('mainControllers', []);
 
-mainController.controller('HomeController', ['$scope',
-  function($scope) {
+mainController.controller('HomeController', ['$scope', '$location', '$anchorScroll',
+  function($scope, $location, $anchorScroll) {
+    $scope.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
+    };
   }]);
